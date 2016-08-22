429cb409d50be77244b46e255fc6c6a495eeaf95
Torsten Scholz <doaschdn@googlemail.com>
2016-08-22 19:58:14 +0200

    Display entered text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+extern crate sfml;
+
+use sfml::graphics::{Color, RenderTarget, RenderWindow, RectangleShape, Transformable, Shape, Font, Text};
+use sfml::window::{Key, VideoMode, event, window_style};
+use sfml::system::{Vector2f};
+
 fn main() {
-    println!("Hello, world!")
+    let mut window = RenderWindow::new(VideoMode::new_init(800, 600, 32),
+                                       "Attext!",
+                                       window_style::CLOSE,
+                                       &Default::default())
+        .unwrap();
+    window.set_vertical_sync_enabled(true);
+
+    let mut input = String::from("");
+
+    let font = Font::new_from_file("arial.ttf").unwrap();
+    let mut text = Text::new().unwrap();
+
+    text.set_character_size(20);
+    text.set_font(&font);
+    text.set_position2f(0f32, 580f32);
+
+    loop {
+        for event in window.events() {
+            match event {
+                event::Closed => return,
+                event::KeyPressed { code: Key::Escape, .. } => return,
+                event::TextEntered { code } => {
+                    input.push(code);
+                    text.set_string(&input);
+                }
+                _ => {}
+            }
+        }
+
+        window.clear(&Color::black());
+        window.draw(&text);
+        window.display();
+    }
 }
